425f91d14da23f9b5043b035f9a30437b9061429
ahma1003@stud.kea.dk <Yazainab12>
2018-12-16 21:50:02 +0100

    scrips changes to server.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -11,3 +11,4 @@ module.exports = {mongoose};
 
 
 // MONGODB_URI: mongodb://heroku_2vx5dwd1:ov686924gnmcbo8v7k636vdr8d@ds135760.mlab.com:35760/heroku_2vx5dwd1
+// https://quiet-temple-14143.herokuapp.com
