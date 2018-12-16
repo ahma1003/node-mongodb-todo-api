0fe56822cecba1d71bee0fb3eb5684a4b621a226
ahma1003@stud.kea.dk <Yazainab12>
2018-12-16 21:33:22 +0100

    no changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 // exported to other files
 module.exports = {mongoose};
+
+
+
+
+
+// MONGODB_URI: mongodb://heroku_2vx5dwd1:ov686924gnmcbo8v7k636vdr8d@ds135760.mlab.com:35760/heroku_2vx5dwd1
